ff01d07121dcb05248b016c326259efa0329e179
CYL <642940617@qq.com>
2018-12-18 22:40:01 +0800

    Update QuickSort.cpp
    adjustment right output,that the same as   the experiment

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 template<class ElemType>
 void QuickSort(ElemType elem[], int low, int high)
 {
+	static int count=0;
 	ElemType e = elem[low];
 	int i = low, j = high;
 	while (i < j)
@@ -14,9 +15,14 @@ void QuickSort(ElemType elem[], int low, int high)
 		if (i < j) elem[j--] = elem[i];
 	}
 	elem[i] = e;  //到这里i和j是相等的
-	for (int k = 0; k <= 7; k++)
+	if (count % 2 == 0)
+	{
+		for (int k = 0; k <= 7; k++)
 		cout << elem[k] << ' ';
-	cout << endl;
+		cout << endl;
+
+	}
+	count++;
 	if (low < i - 1) QuickSort(elem, low, i - 1);
 	if (high > j + 1)QuickSort(elem, j + 1, high);
 }
@@ -24,6 +30,9 @@ int main()
 {
 	int arry[20] = { 49,38,65,97,76,13,49,27 };
 	QuickSort(arry, 0, 7);
+	for (int i = 0; i <= 7; i++)
+		cout << arry[i] << ' ';
+	cout << endl;
 	system("pause");
 	return 0;
 }
